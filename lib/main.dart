681d9f474cfd1b08c9c7b558aa7fd8ef90700e01
Mihail <mikrul85@gmail.com>
2019-10-10 01:50:26 +0300

    2 commit

--- lib/main.dart
+++ lib/main.dart
@@ -67,7 +67,7 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('MyApp'),
+          title: Text('Questionnaire App'),
         ),
         body: _questionIndex < _questions.length
             ? Quiz(
